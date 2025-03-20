14a98c4a21a17faef03cad065c107998475946b3
CarlosBF <cborrellf@outlook.es>
2025-03-20 13:28:17 +0100

    Added image display discretion and a list of all pokemon available

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-const btnInfo = document.getElementById('get-pokemon')
+const selector = document.getElementById('pokemon-select')
 
-btnInfo.addEventListener('click', () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${document.getElementById('pokemon-select').value}`).then(
+document.getElementById('get-pokemon').addEventListener('click', () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${selector.value}`).then(
         (response) =>{
             if(!response.ok){
                 console.error('Ha habido un problema con la obtención de datos')
             }
             return response.json()
     }).then((data) =>{
-        console.log(data)
-        document.getElementById('display').innerHTML = `
+        let display = document.getElementById('display')
+        display.style.display = 'block'
+        display.innerHTML = `
         <div class="section">
             <div class="info">
                 <h3>Name: ${data.name}</h3>
@@ -18,8 +19,8 @@ btnInfo.addEventListener('click', () => {
                 <p><b>Height</b>: ${data.height} inch</p>
                 <p><b>Weight</b>: ${data.weight} pound</p>
             </div>
-            <img src="${data.sprites.other["official-artwork"].front_default}" height=200 class="border">
-            <img src="${data.sprites.other["official-artwork"].front_shiny}" height=200 class="border">
+            ${showImage(data.sprites.other["official-artwork"].front_default)}
+            ${showImage(data.sprites.other["official-artwork"].front_shiny)}
         </div>
         <div class="tables">
             <div class="stats">
@@ -53,6 +54,46 @@ function showMoves(moves) {
     let ans = ''
     for (const move of moves) {
         ans += `<p>${move.move.name}</p>`
+    }
+    return ans
+}
+
+function showImage(image) {
+    if(image != null){
+        return `<img src="${image}" height=200 class="border">`
+    }else{
+        return `<img style="display:none;">`
+    }
+}
+
+selector.innerHTML = ''
+fetchOptions('https://pokeapi.co/api/v2/pokemon')
+
+function fetchOptions(url) {
+    fetch(url).then((response) => {
+        if(!response.ok){
+            console.error('Ha habido un problema con la obtención de la lista de pokemon')
+        }
+        return response.json()
+    }).then((data) => {
+        console.log(data)
+        selector.innerHTML += `
+            ${createOptions(data)}
+        `
+    }).catch((error) => {
+        console.error(error)
+    })
+}
+
+
+function createOptions(data){
+    let ans = ''
+    let results = data.results
+    for (const option of results) {
+        ans += `<option value="${option.name}">${option.name}</option>`
+    }
+    if(data.next != null){
+        fetchOptions(data.next)
     }
     return ans
 }
